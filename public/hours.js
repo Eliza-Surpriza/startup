d8d8b8a834f345a2df2257d8f8962c0cfecd6667
Eliza Freestone <elizarf@byu.edu>
2023-11-11 10:53:28 -0700

    community project data is received

--- public/hours.js
+++ public/hours.js
@@ -2,6 +2,8 @@ window.onload = function() {
     let projects = JSON.parse(localStorage.getItem('projects'));
     let table = document.getElementById('userProjects');
     let tbody = table.createTBody();
+    let communityTable = document.getElementById('communityProjects');
+    let ctbody = communityTable.createTBody();
 
 for (let project of projects) {
     let row = tbody.insertRow();
@@ -21,4 +23,32 @@ for (let project of projects) {
     cell1.textContent = project.projectName;
     cell2.textContent = hours + ":" + minutes + ":" + seconds;
 };
+
+
+fetch('/timer')
+.then(response => response.json())
+.then(data => {
+    for (let project of data) {
+        let row = ctbody.insertRow();
+        let cell1 = row.insertCell();
+        let cell2 = row.insertCell();
+        let cell3 = row.insertCell();
+        
+        console.log(project.time)
+        let time = parseInt(project.time)
+        let seconds = Math.floor((time / 1000) % 60);
+        let minutes = Math.floor((time / (1000 * 60)) % 60);
+        let hours = Math.floor((time / (1000 * 60 * 60)));
+    
+        hours = ('0' + hours).slice(-2);
+        minutes = ('0' + minutes).slice(-2);
+        seconds = ('0' + seconds).slice(-2);
+    
+        cell1.textContent = project.name
+        cell2.textContent = project.projectName;
+        cell3.textContent = hours + ":" + minutes + ":" + seconds;
+    };
+})
+.catch(error => console.error('Error:', error));
+
 };
