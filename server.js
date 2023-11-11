c538c42a6df96aa489ea90aa512e73ccf38d3e72
Eliza Freestone <elizarf@byu.edu>
2023-11-11 10:07:38 -0700

    server can get timer data now

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const app = express();
 app.use(express.json)
 app.use(express.static('public'))
 app.listen(4000, function() {console.log("Server is running")})
+let community = []
+app.post('/timer', function (req, res) {
+    let entry = {
+        name: req.body.name,
+        project: req.body.projectName,
+        time: req.body.number
+      };
+      community.push(entry);
+      console.log(community);
+});
