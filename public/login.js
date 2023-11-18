7dc3f507058bf665ed57e692bc18053e78f92d16
Eliza Freestone <elizarf@byu.edu>
2023-11-18 13:39:02 -0700

    add front end calls for login

--- public/login.js
+++ public/login.js
@@ -1,11 +1,43 @@
 window.onload = function() {
 
-function login() {
-    const nameEl = document.querySelector("#username");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "f.html";
-  }
+// function login() {
+//     const nameEl = document.querySelector("#username");
+//     localStorage.setItem("userName", nameEl.value);
+//     window.location.href = "f.html";
+//   }
   
-  document.querySelector("#loginButton").addEventListener("click", login);
+//   document.querySelector("#loginButton").addEventListener("click", login);
 
-};
+// };
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const name = document.querySelector('#username')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ name: name, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('username', userName);
+    window.location.href = 'focus.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
+}
